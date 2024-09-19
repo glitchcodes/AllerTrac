a1a7359dcd863f8b4a4e9a93dafced575383b17e
Vincent Tampos <56838797+glitchcodes@users.noreply.github.com>
2024-09-19 11:52:39 +0800

    new: add information alert

---
+++ src/components/alert/InfoAlert.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+  import { IonIcon } from "@ionic/vue";
+  import { informationCircle } from "ionicons/icons";
+
+  const props = defineProps<{
+    text: string
+  }>()
+</script>
+
+<template>
+  <div class="flex items-center p-4 text-sm text-blue-800 rounded-lg bg-blue-100 gap-3" role="alert">
+    <ion-icon class="alert-icon" aria-hidden="true" :icon="informationCircle" />
+    <div>
+      {{ props.text }}
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+  ion-icon.alert-icon {
+    font-size: 1.5rem;
+  }
+</style>
