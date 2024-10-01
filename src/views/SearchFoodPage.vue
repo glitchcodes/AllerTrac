17d79d24c644a1f0d96b2c3157954d9d83a77059
Vincent Tampos <56838797+glitchcodes@users.noreply.github.com>
2024-10-01 11:56:25 +0800

    refactor(search): show different placeholders on search bar & layout changes

--- src/views/SearchFoodPage.vue
+++ src/views/SearchFoodPage.vue
@@ -1,39 +1,108 @@
 <script setup lang="ts">
-import { IonPage, IonContent, IonSearchbar } from "@ionic/vue";
-import { useAuthStore } from "@/store/auth";
-import WarningAlert from "@/components/alert/WarningAlert.vue";
+  import { computed } from "vue";
+  import { IonPage, IonContent, IonSearchbar, IonIcon, IonButtons, IonButton, isPlatform } from "@ionic/vue";
+  import { cafeOutline, ellipsisHorizontal, logIn } from "ionicons/icons";
 
-const authStore = useAuthStore();
+  import { useAuthStore } from "@/store/auth";
+  import { useMenuNav } from "@/composables/useMenuNav";
+
+  import WarningAlert from "@/components/alert/WarningAlert.vue";
+
+  const authStore = useAuthStore();
+  const { openUserMenu } = useMenuNav();
+
+  const randomMeal = computed(() => {
+    const placeholders = [
+      'Spaghetti Carbonara',
+      'Chicken Alfredo',
+      'Beef Tacos',
+      'Grilled Salmon',
+      'Veggie Stir-fry',
+      'Margherita Pizza',
+      'Chicken Biryani',
+      'Beef Stroganoff',
+      'Shrimp Scampi',
+      'Vegetable Curry'
+    ];
+
+    const i = Math.floor(Math.random() * placeholders.length);
+
+    return placeholders[i];
+  })
 </script>
 
 <template>
   <ion-page>
+    <ion-header>
+      <!-- Main Toolbar -->
+      <ion-toolbar v-if="isPlatform('ios')">
+        <ion-title>Find Meal</ion-title>
+
+        <ion-buttons slot="primary">
+          <ion-button v-if="authStore._isLoggedIn" @click="openUserMenu">
+            <ion-icon slot="icon-only" :icon="ellipsisHorizontal"></ion-icon>
+          </ion-button>
+          <ion-button v-else router-link="/u/login" shape="round">
+            <ion-icon slot="icon-only" aria-label="Login Button" :icon="logIn"></ion-icon>
+          </ion-button>
+        </ion-buttons>
+      </ion-toolbar>
+      <!-- END Main Toolbar -->
+
+      <!-- Search bar -->
+      <ion-toolbar v-if="isPlatform('ios')">
+        <ion-searchbar :animated="true" :placeholder="randomMeal" :disabled="!authStore._isLoggedIn" />
+      </ion-toolbar>
+      <!-- END Search bar -->
+    </ion-header>
+
     <ion-content class="ion-padding" :fullscreen="true">
-      <div class="bg-primary rounded-lg shadow-lg w-full text-left mb-6 p-5">
+
+      <div class="mb-4">
+        <div class="flex items-center">
+          <ion-icon class="text-2xl primary" aria-hidden="true" :icon="cafeOutline" />
+          <h5 v-if="authStore._isLoggedIn && authStore._user" class="font-bold text-gray-600 ms-2">
+            Hello, {{ authStore._user.first_name }}
+          </h5>
+          <h5 v-else class="font-bold text-gray-600 ms-2">
+            Hello, traveller
+          </h5>
+        </div>
+        <h1 class="text-xl font-bold mt-1">
+          What are you craving today?
+        </h1>
+      </div>
+
+      <!-- Search bar ANDROID -->
+      <div v-if="!isPlatform('ios')" class="mb-4">
+        <ion-searchbar class="search-input"
+                       :animated="true"
+                       :placeholder="randomMeal"
+                       :disabled="!authStore._isLoggedIn"
+        />
+      </div>
+      <!-- END Search bar ANDROID -->
+
+      <!-- Warning: Non-registered users -->
+      <WarningAlert v-if="!authStore._isLoggedIn" class="shadow mb-4">
+        Finding meals are only available to registered users
+      </WarningAlert>
+      <!-- END Warning: Non-registered users -->
+
+      <div class="bg-white rounded-2xl shadow-md w-full text-left mb-6 px-5 py-3">
 
         <div class="flex justify-between items-center">
           <div>
-            <h6 class="text-xl text-white font-bold mb-3">
-              Discover Allergens easily with us!
+            <h6 class="text-primary text-xl font-bold mb-3">
+              Know your meals!
             </h6>
-            <h6 class="text-[11px] text-black">
-              Please search for your meal <br> to see its ingredients.
+            <h6 class="text-[12px]">
+              Please search for your meal to see its ingredients.
             </h6>
           </div>
-            <img class="w-[130px]" src="/images/searchpic.png" alt="search icon">
+            <img class="w-[110px]" src="/images/searchpic.png" alt="search icon">
         </div>
       </div>
-
-      <div class="flex flex-col items-center justify-center bg-white/50 rounded-2xl shadow-2xl p-4 gap-6">
-
-        <WarningAlert v-if="!authStore._isLoggedIn"
-                      class="shadow"
-                      text="Sending ingredients are only available to registered users"
-        />
-        <ion-searchbar></ion-searchbar>
-
-      </div>
-
     </ion-content>
   </ion-page>
 </template>
@@ -41,5 +110,12 @@ const authStore = useAuthStore();
 <style scoped lang="scss">
 .custom-flex {
   height: calc(100% - 16px - 64px - 24px);
+}
+
+ion-searchbar.search-input {
+  --background: white;
+  --border-radius: 0.5rem;
+  --box-shadow: var(--tw-ring-offset-shadow, ), var(--tw-ring-shadow, 0 0 #0000), 0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1);
+  padding: 0;
 }
 </style>
