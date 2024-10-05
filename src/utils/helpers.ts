7948adc8dc5bc963084a728c842a8316f78e8649
Vincent Tampos <56838797+glitchcodes@users.noreply.github.com>
2024-10-05 08:37:23 +0800

    new(helper): add random string generator

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -22,3 +22,16 @@ export const formatToFriendlyDate = (value: string) => {
 
   return formattedDate.format(date);
 }
+
+export const randomString = (length: number): string => {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+
+  let result = '';
+
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result;
+}
