0727d7f573eb260587a909eedd44bb0bea2fad2b
Vincent Tampos <56838797+glitchcodes@users.noreply.github.com>
2024-11-10 15:02:55 +0800

    refactor: remove unused imports

--- src/composables/useGoogleAuth.ts
+++ src/composables/useGoogleAuth.ts
@@ -1,18 +1,8 @@
-import {Capacitor} from "@capacitor/core";
-import {GoogleLoginResponse, SocialLogin} from "@capgo/capacitor-social-login";
-import {alertController} from "@ionic/vue";
-import {alertCircle} from "ionicons/icons";
-
-import {useRouter} from "vue-router";
-import {useAuthStore} from "@/store/auth";
-import {useToastController} from "@/composables/useToastController";
-import FetchError from "@/utils/errors/FetchError";
+import { Capacitor } from "@capacitor/core";
+import { GoogleLoginResponse, SocialLogin } from "@capgo/capacitor-social-login";
+import { alertController } from "@ionic/vue";
 
 export const useGoogleAuth = () => {
-  const router = useRouter();
-  const auth = useAuthStore();
-  const toast = useToastController();
-
   const initialize = async () => {
     // const webClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
     // const mobileClientId = isPlatform('ios') ? import.meta.env.VITE_GOOGLE_CLIENT_ID_IOS : import.meta.env.VITE_GOOGLE_CLIENT_ID
@@ -51,8 +41,6 @@ export const useGoogleAuth = () => {
     });
 
     const response = res.result as GoogleLoginResponse
-
-    console.log(response)
 
     return JSON.stringify({
       email: response.profile.email,
