8d4b844c5ae579fd37f4c965213c6171e14dec7a
Vincent Tampos <56838797+glitchcodes@users.noreply.github.com>
2024-11-17 15:31:50 +0800

    fix: h3 shouldn't be on a button

--- src/components/FactCarousel.vue
+++ src/components/FactCarousel.vue
@@ -25,8 +25,8 @@ try {
     </template>
 
     <div class="rounded-2xl shadow-xl mx-2 p-4 bg-cover bg-center min-h-96 min-w-80 max-h-80 max-w-80 overflow-hidden snap-center flex flex-col-reverse space-y-4 space-y-reverse" style="background-image: url('/images/allergies/allergies.jpg');">
-      <button class="bottom-4 left-4 w-full p-4 bg-[#417776] rounded-lg opacity-90">
-        <h3 class="font-bold text-lg text-[#F4EDEF]">See All</h3>
+      <button class="bottom-4 left-4 w-full p-4 bg-[#417776] rounded-lg opacity-90 font-bold text-lg text-[#F4EDEF]">
+        See all
       </button>
     </div>
   </div>
